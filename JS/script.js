9b2d41aef225678c732a0a91e2b4e39bc37f49ee
mando157 <mando.1572005@gmail.com>
2026-01-21 12:00:13 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+const menuOpenButton = document.getElementById("menu-open-button");
+const menuCloseButton = document.getElementById("menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle the mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+// Close the mobile menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Swiper Testimonials Section
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  spaceBetween: 25,
+  grabCursor: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
